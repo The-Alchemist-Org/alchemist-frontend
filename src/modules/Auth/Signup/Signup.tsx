1e4097caf8e82a6c315ff65635f31af7849758f2
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2024-04-28 19:49:11 +0200

    Add signup page to auth router

---
+++ src/modules/Auth/Signup/Signup.tsx
@@ -0,0 +1,9 @@
+import { FormContainer } from '../components';
+
+import { SignupForm } from './components';
+
+export const Signup = () => (
+  <FormContainer>
+    <SignupForm />
+  </FormContainer>
+);
