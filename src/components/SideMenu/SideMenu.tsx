2a6b46a4793bbeb8cbd3208ee6056d82be149247
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2024-05-09 10:06:11 +0200

    feat: Add SideMenu component and SidebarSubMenu component

---
+++ src/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,69 @@
+import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { cn } from '../../utils/cn';
+import { SidebarSubMenu } from './SidebarSubMenu';
+import { menuItems } from './menu.data';
+
+const settingsRoutes = menuItems.find((item) => item.route === '/settings')?.subMenuItems?.map((item) => item.route);
+
+const isRouteMatch = (pathname: string, itemRoute: string) => {
+  if (pathname.includes('home') && itemRoute === '/') {
+    return true;
+  }
+  if (settingsRoutes?.some((route) => pathname.startsWith(route!)) && itemRoute === '/settings') {
+    return true;
+  }
+
+  return pathname === itemRoute;
+};
+
+export const SideMenu = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const { pathname } = useLocation();
+  return (
+    <div className={`relative h-full transition-width duration-300 ease-in-out ${isMenuOpen ? 'w-64' : 'w-14'}`}>
+
+      {menuItems.map((item) => {
+        const routeMatch = isRouteMatch(pathname, item.route || '');
+        let linkEl;
+        if (item?.subMenuItems) {
+          linkEl = (
+            // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex -- Tooltip workaround
+            <span tabIndex={0}>
+              <SidebarSubMenu menu={item} isMenuOpen={isMenuOpen} isActive={routeMatch} />
+            </span>
+          );
+        } else {
+          linkEl = (
+            <Link
+              className={cn('block py-4 pl-4 text-cool-gray group hover:text-primary hover:bg-primary-50', {
+                'bg-primary text-white': routeMatch,
+              })}
+              to={item?.route ?? ''}
+            >
+              {item.icon}
+              <span className={cn('ml-2 group-hover:text-text-primary', {
+                hidden: !isMenuOpen,
+              })}
+              >
+                {item.name}
+              </span>
+            </Link>
+          );
+        }
+
+        return <div>{linkEl}</div>;
+      })}
+
+      <button
+        type="button"
+        onClick={() => setMenuOpen(!isMenuOpen)}
+        className="p-1 rounded-full border border-solid border-cool-gray bg-white shadow-sm text-cool-gray absolute bottom-20 right-[-1.1rem] duration-300 ease-in-out"
+      >
+        {isMenuOpen ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </button>
+    </div>
+
+  );
+};
---
+++ src/components/SideMenu/SidebarSubMenu.tsx
@@ -0,0 +1,58 @@
+import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { cn } from '../../utils/cn';
+import {
+  DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem,
+} from '../DropDownMenu';
+import { MenuItem } from './menu.data';
+
+export const SidebarSubMenu = (
+  { menu: { name, icon, subMenuItems }, isMenuOpen, isActive }
+  : { menu: MenuItem; isMenuOpen: boolean, isActive?: boolean },
+) => {
+  const [openSubMenu, setOpenSubMenu] = useState(false);
+  const { pathname } = useLocation();
+  const isSubmenuOpen = openSubMenu;
+
+  return (
+    <DropdownMenu open={openSubMenu} onOpenChange={() => setOpenSubMenu((prev) => !prev)}>
+      <DropdownMenuTrigger className={cn(
+        'py-4 pl-4 text-cool-gray hover:text-primary hover:bg-primary-50 w-full text-left flex items-start',
+        {
+          'text-white bg-primary': isSubmenuOpen || isActive,
+        },
+      )}
+      >
+        {icon}
+        <span className={cn('ml-2', {
+          hidden: !isMenuOpen,
+        })}
+        >
+          {name}
+        </span>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="origin-top-right absolute -mt-14 ml-8 w-44 rounded-md shadow-lg bg-white focus:outline-none p-0"
+      >
+        {subMenuItems?.map((subMenuItem) => (
+          <Link to={subMenuItem?.route ?? ''} onClick={() => setOpenSubMenu(false)}>
+            <DropdownMenuItem
+              className={cn(
+                'cursor-pointer block px-2 py-2 text-sm text-text-primary w-full text-left',
+                {
+                  'bg-primary text-white': pathname === subMenuItem.route,
+                  'hover:bg-primary-50': pathname !== subMenuItem.route,
+                },
+              )}
+            >
+              {subMenuItem.icon}
+              <span className="ml-4">
+                {subMenuItem.name}
+              </span>
+            </DropdownMenuItem>
+          </Link>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
